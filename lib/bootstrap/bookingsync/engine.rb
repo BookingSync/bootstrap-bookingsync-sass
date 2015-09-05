b44c093c453dc8a71b2e16d49b617fe838c86d32
Sébastien Grosjean <public@zencocoon.com>
2015-09-05 17:09:46 +0300

    Add engine

---
+++ lib/bootstrap/bookingsync/engine.rb
@@ -0,0 +1,14 @@
+module Bootstrap
+  module BookingSync
+    module Rails
+      class Engine < ::Rails::Engine
+        initializer 'bootstrap-bookingsync-sass.assets.precompile' do |app|
+          %w(stylesheets javascripts fonts images).each do |sub|
+            app.config.assets.paths << root.join('assets', sub).to_s
+          end
+          app.config.assets.precompile << %r(bookingsync/bookingsync-smiles\.(?:eot|svg|ttf|woff2?)$)
+        end
+      end
+    end
+  end
+end
