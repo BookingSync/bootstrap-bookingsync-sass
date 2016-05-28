64adf83021aba8b69e93048b52ee3c925a04112a
Sébastien Grosjean <public@zencocoon.com>
2016-05-28 11:03:41 +0300

    Add Ember forms and modal dependencies

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ var Funnel = require('broccoli-funnel');
 module.exports = {
   name: 'ember-cli-bootstrap-bookingsync-sass',
 
+  included: function(app, parentAddon) {
+    var target = (parentAddon || app);
+    this._super.included(target);
+
+    this.assetsPath = path.join(this.root, 'assets');
+
+    if (process.env.EMBER_CLI_FASTBOOT) {
+      this.ui.writeLine('bootstrap-bookingsync-sass: no JS enabled [FastBoot]');
+    } else {
+      target.import('vendor/ember-cli-bootstrap-bookingsync-sass/bookingsync/form.js');
+      target.import('vendor/ember-cli-bootstrap-bookingsync-sass/bookingsync/stackable.js');
+      target.import('vendor/ember-cli-bootstrap-bookingsync-sass/bookingsync/switch.js');
+    }
+  },
+
   contentFor: function(type) {
     if (type === 'head') {
       return '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700,300">';
@@ -14,10 +29,20 @@ module.exports = {
   },
 
   treeForStyles: function(tree) {
-    var assetsPath = path.join(this.root, 'assets', 'stylesheets');
-    var assetsTree = new Funnel(this.treeGenerator(assetsPath), {
+    var stylesheetsPath = path.join(this.assetsPath, 'stylesheets');
+    var assetsTree = new Funnel(this.treeGenerator(stylesheetsPath), {
       srcDir: '/',
       destDir: '/app/styles'
+    });
+
+    return assetsTree;
+  },
+
+  treeForVendor: function(tree) {
+    var javascriptsPath = path.join(this.assetsPath, 'javascripts');
+    var assetsTree = new Funnel(this.treeGenerator(javascriptsPath), {
+      srcDir: '/',
+      destDir: '/ember-cli-bootstrap-bookingsync-sass'
     });
 
     return assetsTree;
