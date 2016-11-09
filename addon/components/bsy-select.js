867a50019f00df277ba1026026b323e45b9b8d85
Sébastien Grosjean <public@zencocoon.com>
2016-11-09 12:55:55 +0100

    Add bsy-select compnent

---
+++ addon/components/bsy-select.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import layout from '../templates/components/bsy-select';
+
+export default Ember.Component.extend({
+  layout,
+  tagName: "div",
+  classNames: "select"
+});
