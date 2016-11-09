28e0f926437c5c5a9caa4fb8458aed1dea982125
Sébastien Grosjean <public@zencocoon.com>
2016-11-09 13:00:15 +0100

    Add bsy-select-multiple component

---
+++ addon/components/bsy-select-multiple.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import layout from '../templates/components/bsy-select-multiple';
+
+export default Ember.Component.extend({
+  layout,
+  tagName: "div",
+  classNames: "select"
+});
