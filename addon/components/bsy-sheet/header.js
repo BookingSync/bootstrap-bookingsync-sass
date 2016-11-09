2c626b3ae0e05a9b88e57be7eafcb933e7f52168
Sébastien Grosjean <public@zencocoon.com>
2016-11-09 15:29:54 +0100

    Add bsy-sheet component

---
+++ addon/components/bsy-sheet/header.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../../templates/components/bsy-sheet/header';
+
+export default Ember.Component.extend({
+  layout,
+  classNames: 'sheet-header'
+});
