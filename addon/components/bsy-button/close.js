00a97a54eb2278b6cded41c50ba0efb407c1b4cd
Sébastien Grosjean <public@zencocoon.com>
2016-11-09 14:25:38 +0100

    Add bsy-button.close component

---
+++ addon/components/bsy-button/close.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+const { computed } = Ember;
+import layout from '../../templates/components/bsy-button/close';
+
+export default Ember.Component.extend({
+  layout,
+  tagName: "button",
+  classNames: "btn btn-secondary-inverse btn-round",
+
+  click: function() {
+    this.$().prop('disabled', 'disabled');
+  }
+});
