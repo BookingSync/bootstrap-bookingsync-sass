0aa334f57d422e61e6973f02ccaf4d7ff8407dc9
Sébastien Grosjean <public@zencocoon.com>
2016-11-09 12:37:45 +0100

    Add bsy-radio component

--- addon/components/bsy-checkbox.js
+++ addon/components/bsy-checkbox.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-const { computed } = Ember;
 import layout from '../templates/components/bsy-checkbox';
 
 export default Ember.Component.extend({
---
+++ app/components/bsy-radio.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-bootstrap-bookingsync-sass/components/bsy-radio';
