51926fdfe7ede2b5b06b1e24690c586f31299972
Roman <roma.khudyakov@gmail.com>
2017-04-01 15:43:59 +0300

    fix filled and form text

--- addon/components/bsy-textarea.js
+++ addon/components/bsy-textarea.js
@@ -4,7 +4,8 @@ import layout from '../templates/components/bsy-textarea';
 const BsyTextareaComponent = Ember.Component.extend({
   layout,
   tagName: "div",
-  classNames: "form-group"
+  classNames: "form-group",
+  classNameBindings: ["value:filled"]
 });
 
 BsyTextareaComponent.reopenClass({
