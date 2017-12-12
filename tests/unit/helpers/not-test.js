a2be40cd9a297558d5955a4fe9c51b54009ab9de
Jovica Susa <jovicasusa1985@gmail.com>
2017-12-12 14:18:31 +0100

    Added not helper

---
+++ tests/unit/helpers/not-test.js
@@ -0,0 +1,10 @@
+import { not } from '../../../helpers/not';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | not');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = not([42]);
+  assert.ok(result);
+});
