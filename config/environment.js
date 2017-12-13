d674ef3ba41580a050d5cff030f43b2ca7dae6dc
Jovica Susa <jovicasusa1985@gmail.com>
2017-12-13 11:57:45 +0100

    bsy-switch fixes
    * Overide ember-toggle style
    * Configure ember-toggle
    * Remove switch.js from including to the app assets
    * Add bsy-switch to dummy app

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,18 @@
 /*jshint node:true*/
 'use strict';
 
-module.exports = function(/* environment, appConfig */) {
-  return { };
+module.exports = function(environment/*, appConfig */) {
+  var ENV = {
+    environment: environment
+  };
+
+  ENV['ember-toggle'] = {
+    includedThemes: ['light'],
+    excludedThemes: ['flip', 'default', 'flip', 'ios', 'skewed', 'flat'],
+    excludeBaseStyles: false,
+    defaultTheme: 'light',
+    defaultSize: 'small',
+  };
+
+  return ENV;
 };
--- tests/integration/components/bsy-switch-test.js
+++ tests/integration/components/bsy-switch-test.js
@@ -6,19 +6,10 @@ moduleForComponent('bsy-switch', 'Integration | Component | bsy switch', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });"
-
   this.render(hbs`{{bsy-switch}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:"
-  this.render(hbs`
-    {{#bsy-switch}}
-      template block text
-    {{/bsy-switch}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim().search(/Off/) !== -1, true,
+    'should render default Off label');
+  assert.equal(this.$().text().trim().search(/On/) !== -1, true,
+    'should render default On label');
 });
