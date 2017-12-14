614b7206b5025bcfe125eeae3d77733deae444cc
Jovica Susa <jovicasusa1985@gmail.com>
2017-12-14 11:57:29 +0100

    Added bsy-tooltip component

---
+++ addon/components/bsy-tooltip.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from 'ember-tooltips/templates/components/ember-tooltip-base';
+import EmberTooltipComponent from 'ember-tooltips/components/ember-tooltip';
+
+export default EmberTooltipComponent.extend({
+  layout
+});
