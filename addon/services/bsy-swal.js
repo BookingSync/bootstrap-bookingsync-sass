6e1879b3d6781a67388e9d3bac20ac94e908c5e0
Jovica Susa <jovicasusa1985@gmail.com>
2017-12-25 18:28:04 +0100

    Added bsy-swal service

---
+++ addon/services/bsy-swal.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import swal from 'sweetalert';
+
+export default Ember.Service.extend({
+  bsySwal: swal,
+});
---
+++ tests/unit/services/bsy-swal-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:bsy-swal', 'Unit | Service | bsy swal', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let service = this.subject();
+  assert.ok(service);
+});
