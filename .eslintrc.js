b69d191f5b44bb0aef7148358d3ac2cb52b8415e
Sabin <sabin@headed.co>
2017-12-31 09:06:31 +0100

    Add eslint-plugin-ember

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,19 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended'
+  ],
   env: {
     browser: true
   },
   rules: {
+    'ember/use-ember-get-and-set': 0,
+    'ember/named-functions-in-promises': 0,
+    'ember/order-in-routes': 1,
+    'ember/order-in-models': 1,
+    'ember/order-in-controllers': 1,
+    'ember/alias-model-in-controller': 0
   }
 };
